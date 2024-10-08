9c3d5c002f5da936f2cf5de886bbfb8cf9781bf7
jwinkey24 <winkeyj2142@student.faytechcc.edu>
2024-10-08 18:47:57 +0000

    P1HW2

--- P1HW2_winkeyjamil.py
+++ P1HW2_winkeyjamil.py
@@ -11,19 +11,23 @@ print("📍 Budget Travel Tool 📍")
 # Ask user to enter their budget for trip
 budget = float(input("What is your travel budget?: $"))
 
-#Ask user what is their travel destination
 
+#Ask User For Hotel Price
 hotel_price = float(input("How much money do you think you will spend on lodging?:$"))
 
+#Ask user what is their travel destination
 traveling = input("Where are you traveling to?: ")
 
+#Ask User how much money will be spent on gas
 gas_money = float(input("How much money do you think you would spend on gas?:$"))
 
+#Ask User how much money will be spent on food
 eatery = float(input("How much money do you think you will spend on food?:$"))
 
-
+#Seperating the Questions from the calculations
 print("-"*10, "Travel Expense Total", "-"*10)
 
+#Replacing the infomation given an placing it in correct format
 print("Location:$", traveling)
 print("Initial Budget:$",format(budget,".2f"))
 print("Food:$",format(eatery, ".2f"))
@@ -32,6 +36,7 @@ print("Lodging/Accomadations:$", format(hotel_price, ".2f"))
 
 print("-"*20)
 
+#Create and Display variable to get final price after the trip
 final_balance = (budget - (hotel_price + gas_money + eatery))
 
 print("The Remaning Balance is:$", format(final_balance, ".2f"))
