eba5e119ff7c0299ff35a41444e3e227a404a014
jwinkey24 <winkeyj2142@student.faytechcc.edu>
2024-10-17 13:36:44 -0400

    Update P2Lab2_winkeyj2142.py

--- P2Lab2_winkeyj2142.py
+++ P2Lab2_winkeyj2142.py
@@ -24,5 +24,6 @@ print("Largest: ", max(swag_numbers))
 print("The total is: ", sum(swag_numbers))
 
 #To Find Average Add All Numbers And Divide By How Many Numbers Are The Sequence
+#LEN is The count of items
 average = sum(swag_numbers / len(swag_numbers)
 print("The Average of The Total is: ", average)
