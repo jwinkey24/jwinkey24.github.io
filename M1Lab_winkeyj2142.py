b06ffe99d1a9883d4d660323fb7f1e5b4898e0de
jwinkey24 <winkeyj2142@student.faytechcc.edu>
2025-01-27 11:28:43 -0500

    Update M1Lab_winkeyj2142.py

--- M1Lab_winkeyj2142.py
+++ M1Lab_winkeyj2142.py
@@ -1,25 +1,24 @@
 #Have user input their total item amount
 userItems = float(input("How many items do you have?: "))
 itemPrice = 0
-i = 1 
-while i < 100
-if userItems > 100:
-    itemPrice = 4.00
-    continue
-if userItems <= 99:
-    itemPrice = 4.25
-    continue
-if itemPrice <= 49:
-    itemPrice = 4.50
-    continue
-if itemPrice <= 19:
-    itemPrice = 4.75
-    continue
-else:
-    print("I'm sorry you entered the incorrect number of items.")
-
-ItemCost = userItems * itemPrice
-
-print(userItems)
-print(itemPrice)
-print(ItemCost)
+#Create structure for while loop
+i = 1
+#Create a if statement concurrent to the problem
+while i == 1:
+    if userItems >= 100:
+        itemPrice = 4.00
+    elif userItems <= 99 and userItems >= 50:
+        itemPrice = 4.25
+    elif itemPrice <= 49 and userItems >= 20:
+        itemPrice = 4.50
+    elif itemPrice <= 19 and userItems >= 1:
+        itemPrice = 4.75
+    else:
+        print("I'm sorry you entered the incorrect number of items.")
+#Calculate the item cost
+    ItemCost = userItems * itemPrice
+#Print the final results
+    print(f"The amount of items are : {userItems}")
+    print(f"The price of the items are: {itemPrice}")
+    print(f"The Cost of the items are: {ItemCost}")
+    i = int(input("Would you like to run, 1 for yes, 0 for no. "))
